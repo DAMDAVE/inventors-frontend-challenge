a72bd46a65c6c2832508d97c090a318e626dbb22
Omoyajowo David Oyindamola <omoyajowodavidoyindamola@gmail.com>
2024-08-10 20:43:43 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,83 +1,80 @@
-import './App.css';
-import Nav from './Components/Navigation/Nav';
-import Body from './Components/Body/Body';
-import Movies from './Components/Movies/Movie';
-import {useEffect, useState } from 'react';
+import "./App.css";
+import Nav from "./Components/Navigation/Nav";
+import Body from "./Components/Body/Body";
+import Movies from "./Components/Movies/Movie";
+import { useEffect, useState } from "react";
 
+<<<<<<< Updated upstream
 function App () {
 const api_key = '3a0702fe349a3f4b1ddfc57e03faa83d';
 let [page, setPage] = useState(1);
 let [i, setI]= useState(0);
 let [data, setData] = useState(null);
+=======
+function App() {
+  const api_key = "3a0702fe349a3f4b1ddfc57e03faa83d";
+  let [page, setPage] = useState(1);
+  let [i, setI] = useState(0);
+  let [data, setData] = useState(null);
+>>>>>>> Stashed changes
 
-useEffect(
-  () => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(
-            `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&page=${page}`
-          );
-          const movies = await response.json();
-          setData(movies);
-        } catch (error) {
-          console.log('Error fetching data:', error);
-        }
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&page=${page}`
+        );
+        const movies = await response.json();
+        setData(movies);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
 
-      fetchData();
-    }, [api_key, page]
-);
+    fetchData();
+  }, [api_key, page]);
 
-console.log(data);
+  console.log(data);
 
-if (!data || !data.results || data.results.length === 0) {
-  return null; 
-}
+  if (!data || !data.results || data.results.length === 0) {
+    return null;
+  }
 
-let imageURL = `https://image.tmdb.org/t/p/original/${data.results[i].backdrop_path}`;
+  let imageURL = `https://image.tmdb.org/t/p/original/${data.results[i].backdrop_path}`;
 
-const handleBackwardClick = () => {
-  setPage(page -= 1)
-}
+  const handleBackwardClick = () => {
+    setPage((page -= 1));
+  };
 
-const handleForwardClick = () => {
-  setPage(page += 1)
-}
+  const handleForwardClick = () => {
+    setPage((page += 1));
+  };
 
-const handleMovieClick = (index) => {
-  setI(index);
-};
+  const handleMovieClick = (index) => {
+    setI(index);
+  };
 
   return (
-    <div className="landingPage"
-        style={
-          {
-            backgroundImage: `url(${imageURL})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            maxWidth: '100vw',
-            maxHeight: '100vh',
-            overflow: 'auto'
-          }
-        }
-      >
-
-         <Nav />
-         <Body 
-          data={data} 
-          i={i}
-          page={page}
-          forward={handleForwardClick}
-          backward={handleBackwardClick}
-          />
-          <Movies 
-            data={data} 
-            i={i}
-
-            onMovieClick={handleMovieClick}
-          />
+    <div
+      className="landingPage"
+      style={{
+        backgroundImage: `url(${imageURL})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        maxWidth: "100vw",
+        maxHeight: "100vh",
+        overflow: "auto",
+      }}>
+      <Nav />
+      <Body
+        data={data}
+        i={i}
+        page={page}
+        forward={handleForwardClick}
+        backward={handleBackwardClick}
+      />
+      <Movies data={data} i={i} onMovieClick={handleMovieClick} />
     </div>
- )
+  );
 }
 export default App;
-
